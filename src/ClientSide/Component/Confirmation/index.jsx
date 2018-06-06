2f1c4e03d01a939bfef8d78bfc5dd13ec4635bc9
benyoo5222 <benyoo5222@gmail.com>
2018-06-06 23:40:57 +0000

    Changed the state names to reflect the checkboxes

--- src/ClientSide/Component/Confirmation/index.jsx
+++ src/ClientSide/Component/Confirmation/index.jsx
@@ -10,8 +10,8 @@ import Checkout from './checkout.jsx';
 class Checkboxes extends React.Component {
   state = {
     message: 'Waiting for user selection',
-    checkedA: false,
-    checkedB: false
+    payStore: false,
+    payOnline: false
   };
 
   sendAppointmentToServer = (stripeToken) => {
@@ -40,38 +40,11 @@ class Checkboxes extends React.Component {
 
   handleChange = (event) => {
     this.setState({[event.target.value]:true})
-    if (event.target.value === "checkedA"){
-      this.setState({checkedB: false})
+    if (event.target.value === "payStore"){
+      this.setState({payOnline: false})
     } else {
-      this.setState({checkedA: false} )
+      this.setState({payStore: false} )
     }
-  }
-
-  onToken = (token) => {
-    fetch('/save-stripe-token', {
-      method: 'POST',
-      body: JSON.stringify(token),
-    }).then(response => {
-      response.json().then(data => {
-        alert(`We are in business, ${data.email}`);
-      });
-    });
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(event.target)
-    const testData = {
-      stripe: {
-      }
-    }
-    const inputFields = event.target.elements
-    const cardNumber= {...testData.stripe, card_number: inputFields.card_number.value }
-    const expiaryMonth = {...cardNumber, exp_month: inputFields.exp_month.value}
-    const expiaryYear = {...expiaryMonth, exp_year: inputFields.exp_year.value}
-    const stripeToken = {...expiaryYear, cvc: inputFields.cvc.value}
-    console.log(stripeToken)
-
   }
 
   render() {
@@ -87,17 +60,17 @@ class Checkboxes extends React.Component {
         <div id="message-area">{this.state.message}</div>
         {clientServices}
         <Checkbox
-          checked={this.state.checkedA}
+          checked={this.state.payStore}
           onChange={this.handleChange}
-          value="checkedA"
+          value="payStore"
         />
         <span> Pay at store </span>
         <Checkbox
-          checked={this.state.checkedB}
+          checked={this.state.payOnline}
           onChange={this.handleChange}
-          value="checkedB"
+          value="payOnline"
         />
-        {this.state.checkedB ?
+        {this.state.payOnline ?
           <StripeProvider apiKey={Publishable.keyPublishable}>
             <Checkout sendAppointment={this.sendAppointmentToServer} clientInfo={this.props.clientInfo} />
           </StripeProvider>
