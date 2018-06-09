71582cd8f2a801c707c5604d2908c99c7fea34d7
apatheticking <jjlee16@gmail.com>
2018-06-09 19:29:28 +0000

    finished wireframe

--- src/ClientSide/Component/Checkout/index.jsx
+++ src/ClientSide/Component/Checkout/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Moment from 'moment';
 
 class Checkout extends Component{
   constructor(props) {
@@ -10,31 +11,32 @@ class Checkout extends Component{
   }
 
   componentDidMount(){
-    console.log("business info", this.props.business);
-    console.log("selectedServices",this.props.selectedServices);
-    console.log("client info", this.props.clientInfo);
-    console.log("selected appointment", this.props.selectedAppointment);
+    // console.log("business info", this.props.business);
+    // console.log("selectedServices",this.props.selectedServices);
+    // console.log("client info", this.props.clientInfo);
+    // console.log("selected appointment", this.props.selectedAppointment);
   }
 
   render() {
+    let totalPrice = 0;
+    const services = this.props.selectedServices.map(function(service, i) {
+                totalPrice += service.priceCents;
+                return (<p key={i}>{service.description} - <b>${(service.priceCents/100.0).toFixed(2)}</b></p>);
+              });
+    let tax = totalPrice*.15;
     return(
       <Card>
         <CardContent>
-          <p>Thank you for booking an appointment with {this.props.business.name}.</p>
+          <p>Thank you for booking an appointment with <b>{this.props.business.name}</b>.</p>
           <p>Your appointment is located at {this.props.business.address} <br/>
-          and is from {/*this.props.selectedAppointment.start} to {this.props.selectedAppointment.end*/}
-          for the following servies.</p>
+          From: <b>{this.props.selectedAppointment.start.toString().substring(0,21)}</b> <br />
+          To: <b>{this.props.selectedAppointment.end.toString().substring(0,21)}</b></p>
 
-          <ul>
-            {
-              this.props.selectedServices.forEach(function(service) {
-                <li>service</li>
-              })
-            }
-          </ul>
+          <p>for the following servies</p>
+          {services}
+          Total: <b>${((totalPrice + tax)/100.0).toFixed(2)} </b>
 
-
-
+          <p>We've sent you a confirmation, See you soon!</p>
         </CardContent>
       </Card>
     );
